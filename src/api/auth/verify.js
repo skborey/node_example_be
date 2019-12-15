588d63cfca3c68922364505566af20bd3822a00f
Borey <skborey@gmail.com>
2019-12-15 18:09:48 +0700

    remove unused code

--- src/api/auth/verify.js
+++ src/api/auth/verify.js
@@ -3,54 +3,44 @@ const config = require('config');
 const secret = config.get('jwt.secret');
 const InvalidTokenRepo = require('../db/invalidToken.js');
 
-let token = {
+let verifyToken = (req, res, next) => {
 
-  isValidToken: (token) => {
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
 
-    
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
 
-    return true;
-  },
-
-  verifyToken: (req, res, next) => {
-
-    let token = req.headers['x-access-token'] || req.headers['authorization'];
-
-    if (token.startsWith('Bearer ')) {
-      // Remove Bearer from string
-      token = token.slice(7, token.length);
-    }
-
-    if (token) { // cehck it is not in invalidToken
-      InvalidTokenRepo.exists({'token': token}, (err, r) => {
-        if (err || r) {
-          return res.json({
-            success: false,
-            message: 'Token is not valid'
-          });
-        } else {
-          jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-              return res.json({
-                success: false,
-                message: 'Token is not valid'
-              });
-            } else {
-              req.decoded = decoded;
-              next();
-            }
-          });
-        }
-      });
-    } else {
-      return res.json({
-        success: false,
-        message: 'Auth token is not supplied'
-      });
-    }
+  if (token) { // cehck it is not in invalidToken
+    InvalidTokenRepo.exists({'token': token}, (err, r) => {
+      if (err || r) {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        jwt.verify(token, secret, (err, decoded) => {
+          if (err) {
+            return res.json({
+              success: false,
+              message: 'Token is not valid'
+            });
+          } else {
+            req.decoded = decoded;
+            next();
+          }
+        });
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
+    });
   }
 }
 
 module.exports = {
-  verifyToken: token.verifyToken
+  verifyToken: verifyToken
 }
